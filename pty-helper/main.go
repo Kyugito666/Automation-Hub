2b2d58a37b8f0acd4c78dc0a5237e9f748660db3
Kyugito <113055222+Kyugito666@users.noreply.github.com>
2025-10-24 19:24:59 +0700

    Create main.go

---
+++ pty-helper/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"github.com/creack/pty" // Library PTY
+	"golang.org/x/term"    // Library TTY Raw Mode
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Usage: pty-helper <command> [args...]")
+		os.Exit(1)
+	}
+	
+	command := os.Args[1]
+	args := os.Args[2:]
+
+	// Buat perintah exec
+	cmd := exec.Command(command, args...)
+
+	// Mulai perintah di dalam PTY
+	ptmx, err := pty.Start(cmd)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting PTY: %v\n", err)
+		os.Exit(1)
+	}
+	defer ptmx.Close()
+
+	// Set STDIN ke RAW mode
+	// Ini adalah KUNCI untuk 'y/n'
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error setting STDIN to raw mode: %v\n", err)
+		os.Exit(1)
+	}
+	// Kembalikan TTY state saat program exit
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	// Handle window resize
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGWINCH)
+	go func() {
+		for range ch {
+			pty.InheritSize(os.Stdin, ptmx)
+		}
+	}()
+	ch <- syscall.SIGWINCH // Trigger sekali saat start
+
+	// Sambungkan I/O
+	// Salin output PTY (bot) ke STDOUT (C#)
+	go io.Copy(os.Stdout, ptmx)
+	// Salin input STDIN (user) ke PTY (bot)
+	go io.Copy(ptmx, os.Stdin)
+
+	// Tunggu proses selesai
+	if err := cmd.Wait(); err != nil {
+		// Keluar dengan exit code dari child process
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
+		}
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
